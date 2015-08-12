b4163b79e464a4dc216fbfeed80a908479b6e02b
Boyd Dames <JustDrawCats@gmail.com>
2015-08-12 17:08:59 +0200

    Enable React addons

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module RethinkdbReactRails
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     # config.active_record.raise_in_transactional_callbacks = true
+
+    # Enable React addons
+    config.react.addons = true
   end
 end
